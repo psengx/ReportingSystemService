44a5800ce73940f23df79437b683941468ee4b67
psengx <psengx@gmail.com>
2026-03-29 15:51:43 +0300

    Миграции по созданию таблиц в БД

---
+++ Migrations/20260329115855_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportingSystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    From = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    To = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportResponses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReportRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ViewsCount = table.Column<int>(type: "integer", nullable: false),
+                    PaymentsCount = table.Column<int>(type: "integer", nullable: false),
+                    Ratio = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportResponses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportRequests");
+
+            migrationBuilder.DropTable(
+                name: "ReportResponses");
+        }
+    }
+}
