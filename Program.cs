2afb5ba7edc4440407a7715205123ad2b6cf96a3
psengx <psengx@gmail.com>
2026-03-29 23:56:57 +0300

    Зарегистрированы сервисы RabbitMq, добавлено применение миграций при запуске приложений

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-
 using Microsoft.EntityFrameworkCore;
 using ReportingSystemService.Infrastucture;
+using ReportingSystemService.Infrastucture.Messaging;
 
 namespace ReportingSystemService
 {
@@ -10,20 +10,21 @@ namespace ReportingSystemService
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<AddDbContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddHostedService<ReportWorker>();
+            builder.Services.AddSingleton<RabbitMqProducer>(); // Регистрируем RabbitMqProducer как синглтон
+            builder.Services.AddHostedService<RabbitMqConsumer>(); // Регистрируем RabbitMqConsumer как фоновую службу
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
+            using (var scope = app.Services.CreateScope()) // Авто-применение миграций при запуске приложения
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AddDbContext>();
+                db.Database.Migrate();
+            }
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
