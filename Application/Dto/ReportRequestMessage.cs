5bf53e3a30a4956623e75b41bb038a9da09adb04
psengx <psengx@gmail.com>
2026-03-29 23:49:11 +0300

    Написана Dto для отправки ReportRequest в виде сообщения на обработку внутри consumer

---
+++ Application/Dto/ReportRequestMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportingSystemService.Application.Dto
+{
+    public class ReportRequestMessage // Dto для передачи данных о запросе в Producer
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid OrderId { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
