d779f09a7f2eba2e1a1df7e440cba41e8bb58390
psengx <psengx@gmail.com>
2026-03-29 15:51:57 +0300

    Удалена ненужная Dto

--- Application/Dto/GetReportResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ReportingSystemService.Application.Dto
-{
-    public class GetReportResponse
-    {
-        public decimal Ratio { get; set; }
-        public int PaymentsCount { get; set; }
-    }
-}
