b387ef854ceb159c405db561ec619a71ce43b69a
psengx <psengx@gmail.com>
2026-03-30 21:13:51 +0300

    Написана модель для окончательного ответа (данные отчета из ТЗ)

---
+++ Models/ReportResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ReportingSystemService.Models
+{
+    public class ReportResponse
+    {
+        public decimal Ratio { get; set; }
+        public int PaymentsCount { get; set; }
+    }
+}
