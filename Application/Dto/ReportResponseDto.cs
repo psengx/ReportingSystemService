21bcf6822d10b816acf79d0d66fb907bfc0d04e3
psengx <psengx@gmail.com>
2026-03-30 21:15:00 +0300

    Написана Dto для GET-запроса

---
+++ Application/Dto/ReportResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using ReportingSystemService.Models;
+
+namespace ReportingSystemService.Application.Dto
+{
+    public class ReportResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public ReportResponseEntity? ReportResponse { get; set; }
+    }
+}
